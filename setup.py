cc0090749a13189255c82e9a8c743f240ae7b6af
LPantolini <lorenzo.pantolini@hotmail.com>
2020-02-12 13:47:33 +0100

    Increase version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='maboss_test',
-    version="1.0.0a1",
+    version="1.0.0a3",
     author="Lorenzo Pantolini",
     author_email="lorenzopantolini@protonmail.com",
     description="A testing library for MaBoSS models",
